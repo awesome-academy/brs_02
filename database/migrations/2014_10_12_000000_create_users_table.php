774776d653c7b11a2d899d5e74f24a6efc7235d6
ThinhVinh <vinhthinh026@gmail.com>
2019-01-04 03:00:25 +0700

    pull routing and view public

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+<<<<<<< HEAD
+=======
+            $table->string('username');
+>>>>>>> accef7c... pull login admin
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+<<<<<<< HEAD
+=======
+            $table->integer('active');
+            $table->string('avatar');
+            $table->string('address');
+>>>>>>> accef7c... pull login admin
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,42 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::namespace('Bookreview')->group(function () {
+    Route::get('/', [
+        'uses' => 'IndexController@index',
+        'as' => 'bookreview.index.index'
+    ]);
+});
+<<<<<<< HEAD
+=======
+
+Route::namespace('Admin')->prefix('admin')->group(function () {
+
+    Route::get('/', [
+        'uses' => 'IndexController@index',
+        'as' => 'admin.index.index'
+    ])->middleware('auth');
+
+    Route::get('/cat', [
+        'uses' => 'CategoryController@index',
+        'as' => 'admin.cat.index'
+    ])->middleware('auth');
+
+    Route::get('/login',[
+        'uses' => 'LoginController@getLogin',
+        'as' => 'login',
+    ]);
+
+    Route::post('/login',[
+        'uses' => 'LoginController@postLogin',
+        'as' => 'admin.login.index',
+    ]);
+
+    Route::get('logout',[
+        'uses'=>'LoginController@logOut',
+        'as'=>'admin.logout'
+    ])->middleware('auth');
+
+});
+>>>>>>> accef7c... pull login admin
